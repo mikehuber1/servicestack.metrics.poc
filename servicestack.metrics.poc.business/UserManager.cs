9a99b64b10e93063dbe5d0c5447fb3cabc3b5e95
Mike Huber <mike.huber@exanic.ch>
2024-10-01 11:42:53 +0200

    Add activity

--- servicestack.metrics.poc.business/UserManager.cs
+++ servicestack.metrics.poc.business/UserManager.cs
@@ -1,13 +1,17 @@
-﻿namespace servicestack.metrics.poc.business;
+﻿using System.Diagnostics;
+
+namespace servicestack.metrics.poc.business;
 
 public class UserManager : IUserManager
 {
+    private static ActivitySource ActivitySource = new("POC");
+
     public string GetUserInfo(string username)
     {
-        
-        
-        
+        using var activity = ActivitySource.StartActivity("read user store");
+
         Thread.Sleep(500);
+
         return "Peter";
     }
 }
